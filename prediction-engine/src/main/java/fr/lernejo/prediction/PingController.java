50537b647c0cd3cda5a55bcaac27fffe30ab2aec
widedhajjar <hajjar@et.esiea.fr>
2022-01-06 16:24:32 +0100

    API de prédiction

--- prediction-engine/src/main/java/fr/lernejo/prediction/PingController.java
+++ prediction-engine/src/main/java/fr/lernejo/prediction/PingController.java
@@ -1,7 +1,12 @@
 package fr.lernejo.prediction;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 public class PingController {
@@ -10,4 +15,19 @@ public class PingController {
     String ping() {
         return "OK";
     }
+
+    @GetMapping(path = "/temperature")
+    public Object temperature(@RequestParam String country){
+        final TemperatureService temperatureService = new TemperatureService();
+        ArrayList<TemperatureDay> tempList= new ArrayList<>();
+        try{
+            tempList.add(new TemperatureDay("2021-12-04", temperatureService.getTemperature(country)));
+            tempList.add(new TemperatureDay("2021-12-05", temperatureService.getTemperature(country)));
+            return new Temperature(country, tempList);
+        }catch (UnknownCountryException error){
+            return new ResponseEntity<>(error.getMessage(),HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+
+
 }
---
+++ prediction-engine/src/main/java/fr/lernejo/prediction/TemperatureDay.java
@@ -0,0 +1,11 @@
+package fr.lernejo.prediction;
+
+public class TemperatureDay {
+    public String date;
+    public Number temperature;
+
+    public TemperatureDay(String date, Number temperature) {
+        this.date = date;
+        this.temperature = temperature;
+    }
+}
